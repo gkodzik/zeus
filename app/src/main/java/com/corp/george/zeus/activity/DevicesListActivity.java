b63405b8baa307984075e74ee5dae52195a73c93
Grzegorz Kodzik <gkodzik@MASmobileDev.local>
2017-09-25 10:47:01 +0200

    added layout manager and fixed cell height

--- app/src/main/java/com/corp/george/zeus/activity/DevicesListActivity.java
+++ app/src/main/java/com/corp/george/zeus/activity/DevicesListActivity.java
@@ -1,11 +1,13 @@
 package com.corp.george.zeus.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.corp.george.zeus.R;
 import com.corp.george.zeus.adapter.DevicesAdapter;
 import com.corp.george.zeus.data.DummyDataProvider;
+import com.corp.george.zeus.devices.SmartSwitch;
 
 public class DevicesListActivity extends BaseActivity {
 
@@ -16,12 +18,9 @@ public class DevicesListActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices_list);
         recyclerView = (RecyclerView) findViewById(R.id.devices_recycler);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        recyclerView.setAdapter(new DevicesAdapter<>(this, DummyDataProvider.getDummyDevices(10)));
+		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+		recyclerView.setLayoutManager(layoutManager);
+		DevicesAdapter<SmartSwitch> adapter = new DevicesAdapter<>(this, DummyDataProvider.getDummyDevices(10));
+		recyclerView.setAdapter(adapter);
     }
 }
